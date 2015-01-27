03538b2c9dba88f959ab8a7da7a0996af8a56cfd
ywk253100 <ywk253100@163.com>
2015-01-27 22:53:57 +0800

    utf-8

---
+++ src/main/java/com/guess/dao/impl/UserCreateQuestionDaoImpl.java
@@ -0,0 +1,11 @@
+package com.guess.dao.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.guess.dao.UserCreateQuestionDao;
+import com.guess.model.UserCreateQuestion;
+
+@Component
+public class UserCreateQuestionDaoImpl extends BaseDaoImpl<UserCreateQuestion, String> implements UserCreateQuestionDao{
+
+}
