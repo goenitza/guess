c53e129a28e78d509092e7fee537dcf3ab490950
ywk253100 <ywk253100@163.com>
2015-01-27 22:53:48 +0800

    utf-8

---
+++ src/main/java/com/guess/dao/impl/QuestionCategoryDaoImpl.java
@@ -0,0 +1,11 @@
+package com.guess.dao.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.guess.dao.QuestionCategoryDao;
+import com.guess.model.QuestionCategory;
+
+@Component
+public class QuestionCategoryDaoImpl extends BaseDaoImpl<QuestionCategory, String> implements QuestionCategoryDao{
+
+}
